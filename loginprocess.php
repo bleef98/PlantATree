6c6b5aa2db4c85aac889a425d88915c5f2ee4cc5
Jeeerus <jairusmaningas@gmail.com>
2019-05-23 15:30:53 +1200

    login 1.3

--- loginprocess.php
+++ loginprocess.php
@@ -8,12 +8,11 @@
     $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 
 	// get name and password passed from client
-	$email = $_POST['email'];
-    $pwd = $_POST['pwd'];
-    
-    echo $email;
+    echo "HELLO";
+    echo $_POST['email'];
 
-	
+    
+    /*
 	// Checks if given username exists within the table
 	$sqlEmailCheck = "SELECT user_email FROM plantatree WHERE user_email='$email';";
 	$sqlQueryEmail = mysqli_query($conn, $sqlEmailCheck);
@@ -28,12 +27,12 @@
 		$sqlResultPassword = mysqli_fetch_assoc($sqlQueryPassword);
 
 		if($sqlResultPassword["user_password"] == $pwd){
-			// Password and Username combination is correct. Now corresponding email
-			header("home.html"); /* Redirect browser */
+			header("home.html"); 
             exit();
 		}
 		else{
 			echo "Password is incorrect";
 		}
-	}
+    }
+    */
 ?>
