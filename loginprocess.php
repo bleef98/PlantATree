77fb2af9c68dc3a30d2eaabb214495720874a3ff
Jeeerus <jairusmaningas@gmail.com>
2019-05-23 15:43:04 +1200

    login 1.7

--- loginprocess.php
+++ loginprocess.php
@@ -14,27 +14,5 @@
     echo $email;
     echo $pwd;
 
-	
-	// Checks if given username exists within the table
-	$sqlEmailCheck = "SELECT user_email FROM plantatree WHERE user_email='$email';";
-	$sqlQueryEmail = mysqli_query($conn, $sqlEmailCheck);
-	$sqlResultEmail = mysqli_fetch_assoc($sqlQueryEmail);
 
-	if(empty($sqlResultEmail["user_email"])){
-		echo "Email does not exist";
-	}
-	else { // Perform password check
-		$sqlPasswordCheck = "SELECT user_passwords FROM plantatree WHERE user_name='$email';";
-		$sqlQueryPassword = mysqli_query($conn, $sqlPasswordCheck);
-		$sqlResultPassword = mysqli_fetch_assoc($sqlQueryPassword);
-
-		if($sqlResultPassword["user_password"] == $pwd){
-			// Password and Username combination is correct. Now corresponding email
-			header("home.html"); 
-            exit();
-		}
-		else{
-			echo "Password is incorrect";
-		}
-	}
 ?>
