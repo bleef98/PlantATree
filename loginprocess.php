8a24c2a0760cc8a4f24af0755eadef4f7cb554e0
Jeeerus <jairusmaningas@gmail.com>
2019-05-23 15:53:07 +1200

    login 1.9

--- loginprocess.php
+++ loginprocess.php
@@ -9,12 +9,12 @@
 
 	// get name and password passed from client
 	$email = $_POST['email'];
-    $pwd = $_POST['pwd'];
+    $pwd = $_POST['password'];
     
     echo $email;
     echo $pwd;
 
-    $sqlCheckEmail = "SELECT user_name FROM plantatree WHERE user_name='jairus@gmail.com';";
+    $sqlCheckEmail = "SELECT user_email FROM plantatree WHERE user_email='jairus@gmail.com';";
     $sqlQueryEmail = mysqli_query($conn, $sqlCheckEmail);
     $sqlEmailResult = mysqli_fetch_assoc($sqlQueryEmail);
 
