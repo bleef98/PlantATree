60ee2892ba1e1e372a9cec1049e06bb2d698202a
Jeeerus <jairusmaningas@gmail.com>
2019-05-23 13:11:39 +1200

    E

--- registerprocess.php
+++ registerprocess.php
@@ -9,7 +9,7 @@
 
 <body>
     <?php
-    echo "Register Process PHP";
+    echo "Register Process PHP<br>";
 
     $dbServername = "remotemysql.com:3306";
     $dbUsername = "DCrftK3aaZ";
@@ -18,28 +18,22 @@
 
     $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 
-    $sqlCreateTable = "CREATE TABLE IF NOT EXISTS plantatree (
+    $sqlCreateTable = "CREATE TABLE IF NOT EXISTS testree (
         f_name varchar(30), 
-        l_name varchar(256), 
+        l_name varchar(30), 
         user_email varchar(30), 
         user_pwd varchar(30), 
         user_company varchar(30));";
-    /*
-    $sqlCreateTable = "CREATE TABLE IF NOT EXISTS plantATree(
-                            fName varchar(30), 
-                            lName varchar(30), 
-                            userEmail varchar(30), 
-                            userPWD varchar(30, 
-                            userCompany varchar(30));";*/
 
     mysqli_query($conn, $sqlCreateTable);
-
+    
+/*
     $sqlPostDB = "INSERT INTO plantatree (f_name, l_name, user_email, user_pwd, user_company)
                     VALUES('$_POST['fName']', '$_POST['lName']', '$_POST['email']', '$_POST['pwd']', '$_POST['company']');";
     
-    mysqli_query($conn, $sqlPostDB);
+    mysqli_query($conn, $sqlPostDB);*/
 
-    echo "change 1.6";
+    echo "change 1.7";
     echo $_POST['fName'];
     
     
