39e8f87b33591475c557557eb8139f32676a9dd4
Alexis Elloso <ellosoalexis@gmail.com>
2019-05-26 22:09:57 +1200

    merge

--- loginprocess.php
+++ loginprocess.php
@@ -1,68 +1,12 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-
-<html>
-
-<head>
-    <title>Login</title>
-    <meta http-wquiv="Content-Type" content="text/html; charset=utf-8" />
-</head>
-
-<body>
-
-    <?php
-    $dbServername = "remotemysql.com:3306";
-=======
 <!--file data.php -->
 <?php
 	$dbServername = "remotemysql.com:3306";
->>>>>>> 692fcc7077cbc894075957df937991388cbb6af2
     $dbUsername = "DCrftK3aaZ";
     $dbPassword = "sHp1bqQVIC";
     $dbName = "DCrftK3aaZ";
 
     $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 
-<<<<<<< HEAD
-    $email = $_POST['email'];
-    $pwd = $_POST['pwd'];
-
-    $sqlEmailCheck = "SELECT user_email FROM plantatree WHERE user_email='$email';";
-    $sqlQueryEmail = mysqli_query($conn, $sqlEmailCheck);
-    $sqlResultEmail = mysqli_fetch_assoc($sqlQueryEmail);
-
-    if (empty($sqlResultEmail["user_email"])) {
-        echo "That email is incorrect/not registered";
-    } 
-    
-    else{ // Perform password check
-        $sqlPasswordCheck = "SELECT user_pwd FROM plantatree WHERE user_email='$email';";
-        $sqlQueryPassword = mysqli_query($conn, $sqlPasswordCheck);
-        $sqlResultPassword = mysqli_fetch_assoc($sqlQueryPassword);
-
-        if (empty($sqlResultPassword["user_pwd"]) {
-            echo "Password is incorrect";
-        }
-
-        else{
-            if(isset($_POST["submit"])){
-                header('Location:home.html');
-            }
-            
-        }
-    }
-
-
-    ?>
-
-
-
-
-</body>
-
-
-</html>
-=======
 	// get name and password passed from client
 	$email = $_POST['email'];
     $pwd = $_POST['password'];
@@ -92,4 +36,3 @@
         }
 	}
 ?>
->>>>>>> 692fcc7077cbc894075957df937991388cbb6af2
